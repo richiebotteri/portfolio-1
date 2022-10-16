293dfc8ef7e3c6ac94ed850087418cdd4d963e72
richiebotteri <richie.botteri@stud.noroff.no>
2022-10-16 17:10:13 +0200

    made everything responsive

---
+++ js/index.js
@@ -0,0 +1,55 @@
+const modal = document.querySelector(".modal");
+const projectBanners = document.querySelectorAll(".project-banner");
+
+// Listening for click event to toggle modal
+
+modal.addEventListener("click", function (event) {
+   if (event.target.classList.contains("modal")) {
+      event.target.classList.add("hide-element");
+   }
+});
+
+projectBanners.forEach(function (projectBanner) {
+   projectBanner.addEventListener("click", function (event) {
+      if (event.target.classList.contains("cta")) {
+         modal.classList.remove("hide-element");
+         if (event.target.dataset.id == 3) {
+            modal.innerHTML = `
+            <section class="section-container flex-col clr-white flex-ctr-xy flex-gap-40 bg-black">
+              <img class="modal-image" src="/images/rainyday-website.png" alt="" />
+              <h3>Cross-Course Project</h3>
+              <p>Rainydays is a e-commerce website we where tasked to create that sells rain jackets. This using html, CSS and JavaScript</p>
+              <div class="flex flex-gap-40 flex-ctr-y">
+                <a href="https://heartfelt-mochi-956b4b.netlify.app" class="cta bg-white flex flex-ctr-xy cursor-ptr">Live server</a>
+                <a href="https://github.com/richiebotteri/rainydays-wpapi-ca" class="social-icons clr-white cursor-ptr"><i class="fa-brands fa-square-github"></i></a>
+             </div>
+          </section>
+          `;
+         } else if (event.target.dataset.id == 2) {
+            modal.innerHTML = `
+            <section class="section-container flex-col clr-white flex-ctr-xy flex-gap-40 bg-black">
+               <img class="modal-image" src="/images/museum-website.png" alt="Museum Website" />
+               <h3>Semester Project 1:</h3>
+               <p>Community science museum is an website we where tasked to create using html and CSS, and is design to inform and attract visitors to an local museum</p>
+               <div class="flex flex-gap-40 flex-ctr-y">
+                <a href="https://inspiring-valkyrie-0707de.netlify.app" class="cta bg-white flex flex-ctr-xy cursor-ptr">Live server</a>
+                <a href="https://github.com/richiebotteri/Oslo-Museum-Project-Repo" class="social-icons clr-white cursor-ptr"><i class="fa-brands fa-square-github"></i></a>
+              </div>
+            </section>
+          `;
+         } else if (event.target.dataset.id == 1) {
+            modal.innerHTML = `
+          <section class="section-container flex-col clr-white flex-ctr-xy flex-gap-40 bg-black">
+              <img class="modal-image" src="/images/blog-website.png" alt="" />
+              <h3>Project Exam 1</h3>
+              <p>In this website we where tasked to design and build a blog of choice to showcase our Html, CSS, JavaScript and Wordpress skills. I choose to create a Fitness/Nutrition blog for this assignment</p>
+              <div class="flex flex-gap-40 flex-ctr-y">
+                <a href="https://cheerful-pixie-956113.netlify.app" class="cta bg-white flex flex-ctr-xy cursor-ptr">Live server</a>
+                <a href="https://github.com/Noroff-FEU-Assignments/project-exam-1-richiebotteri" class="social-icons clr-white cursor-ptr"><i class="fa-brands fa-square-github"></i></a>
+              </div>
+          </section>
+        `;
+         }
+      }
+   });
+});
